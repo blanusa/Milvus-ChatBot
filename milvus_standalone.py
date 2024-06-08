46d6c19aeeab6fde15335ea536ab0b8c5cd48417
dusankg <vlada2002blanusa@gmail.com>
2024-06-08 22:49:13 +0200

    RADI C U CRUDU

--- milvus_standalone.py
+++ milvus_standalone.py
@@ -1,10 +1,68 @@
-from pymilvus import connections, utility, MilvusException
+from pymilvus import connections, utility, MilvusException, MilvusClient, Collection
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import httpx
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
 
 app = FastAPI()
 
+MILVUS_HOST = 'standalone'
+MILVUS_PORT = 19530
+
 connections.connect(host="standalone", port="19530")
+
+collection_test = Collection(name="test_name")
+
+#collection_bus_routes = Collection(name="BusRoutes")
+
+
+index_params = {
+    "index_type": "IVF_FLAT",
+    "metric_type": "L2",
+    "params": {
+        "nlist": 100
+    }
+}
+
+#collection_bus_routes.create_index(
+# field_name="Vector",
+#  index_params=index_params,
+#  index_name="busroutes_index"
+#)
+
+#collection_test.create_index(
+#  field_name="vector_field",
+#  index_params=index_params,
+#  index_name="busroutes_index"
+#)
+
+#collection_bus_routes.load()
+collection_test.load()
+
+@app.get("/collections/getvector1/{vector_id}")
+async def get_vector(
+    vector_id: int
+):
+    try:
+        client = MilvusClient(uri=f"http://{MILVUS_HOST}:{MILVUS_PORT}", token="root:Milvus")
+
+        vector_data = client.get(collection_name="collection_test", ids=[vector_id])
+
+        if vector_data:
+            vector_dict = {
+                "id": vector_id,
+                "test_field": vector_data[0]["description"],
+            }
+            return JSONResponse(content={"vector_data": vector_dict})
+        else:
+            return JSONResponse(content={"message": "Vector not found"}, status_code=404)
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
 
 @app.get("/")
 async def func():
@@ -21,6 +79,45 @@ async def func():
         response = await client.get("http://host.docker.internal:9099/hi")
         return response.json()
 
+
+@app.get("/get-entity-count/")
+async def get_entity_count():
+    try:
+        count = collection.num_entities
+        return {"message": "Count of entities in collection", "count": count}
+    except Exception as e:
+        return {"message": "Error occurred while getting entity count:", "error": str(e)}
+
+
+@app.get("/test-milvus-connection/")
+async def test_milvus_connection():
+    try:
+        # Check if connected to Milvus
+        client = MilvusClient(uri=f"http://{MILVUS_HOST}:{MILVUS_PORT}", token="root:Milvus")
+        status = client.get_collection_stats(collection_name="test_name")
+        return {"message": "Connected to Milvus", "status": status}
+    except Exception as e:
+        return {"message": "Error occurred during Milvus connection:", "error": str(e)}
+
+class Item(BaseModel):
+    embedding : List[float]
+    name: List[str]
+
+@app.post("/insertText")
+async def insertText(text : Item):
+    try:
+        test_collection = "text_collection"
+        collection = Collection(name=test_collection)
+        embedding = text.embedding
+        name = text.name
+        data = [[embedding],name]
+        mr = collection.insert(data)
+        print(f"Insert result: {mr}")
+        
+        # Step 6: Flush to persist data
+        collection.flush()
+    except Exception as e:
+        return {"message": "Error occurred during Milvus connection:", "error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
