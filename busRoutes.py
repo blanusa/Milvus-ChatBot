d26a9178952d4e0a757ef0ca470e17c65fd78cfc
blanusa <103044122+blanusa@users.noreply.github.com>
2024-06-10 01:15:58 +0200

    Full CSV inserted

--- busRoutes.py
+++ busRoutes.py
@@ -18,10 +18,15 @@ DIMENSION = 300  # Embeddings size
 
 nlp = spacy.load('en_core_web_lg')
 connections.connect(host="standalone", port="19530")
+# Street,Bus Line,Departure Time,RouteDescription,Route Duration (minutes)
 
 fields = [
     FieldSchema(name='id', dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name='Street', dtype=DataType.VARCHAR, max_length=500),
+    FieldSchema(name='BusLine', dtype=DataType.VARCHAR, max_length=500),
+    FieldSchema(name='DepartureTime', dtype=DataType.VARCHAR, max_length=1000),
     FieldSchema(name='RouteDescription', dtype=DataType.VARCHAR, max_length=1000),
+    FieldSchema(name='RouteDuration', dtype=DataType.VARCHAR,max_length=100),
     FieldSchema(name='embedding', dtype=DataType.FLOAT_VECTOR, dim=DIMENSION)
 ]
 schema = CollectionSchema(fields=fields,enable_dynamic_fields=True, primary_field='id')
@@ -43,33 +48,38 @@ def csv_load(file_path, encoding='utf-8'):
         for row in reader:
             if '' in (row[3], row[3]):
                 continue
-            yield (row[3], row[3])
+            yield (row[0],row[1],row[2], row[3],row[4])
 
 
 def embed_insert(data):
-    #embeds = transformer.encode(data[1]) 
-    print(data[1][0],"\n",data[0][0])
-    print("---------------------------------------")
-    doc = nlp(data[1][0])
+    doc = nlp(data[5][0])
     ins = [
             [data[0][0]],
+            [data[1][0]],
+            [data[2][0]],
+            [data[3][0]],
+            [data[4][0]],
             [doc.vector]
     ]
     BusDepartCollection.insert(ins)
 
-
-data_batch = [[],[]]
+data_batch = [[],[],[],[],[],[],[]]
 
 count = 0
+    #Street,BusLine,DepartureTime,RouteDuration,RouteDescription = 
 
-for RouteDescription,RouteDescription1 in csv_load("novi_sad_bus_departure_times.csv"):
-    print(RouteDescription,RouteDescription1)
-    data_batch[0].append(RouteDescription)
-    data_batch[1].append(RouteDescription1)
+i = 0
+for Street,BusLine,DepartureTime,RouteDuration,RouteDescription in csv_load("novi_sad_bus_departure_times.csv"):
+    data_batch[0].append(Street)
+    data_batch[1].append(BusLine)
+    data_batch[2].append(DepartureTime)
+    data_batch[3].append(RouteDuration)
+    data_batch[4].append(RouteDescription)
+    data_batch[5].append(RouteDescription)
     #if len(data_batch[0]) % BATCH_SIZE == 0:
     embed_insert(data_batch)
-    data_batch = [[],[]]
-    count += 1
+    data_batch = [[],[],[],[],[],[],[]]
+    
 
 
 if len(data_batch[0]) != 0:
