2bfce7e1411740e895734448496433f067a4de49
Nemanja Ranitovic <nemanjaranit@gmail.com>
2024-06-10 02:14:38 +0200

    Full Crud implemented

--- milvus_standalone.py
+++ milvus_standalone.py
@@ -14,7 +14,6 @@ MILVUS_HOST = 'standalone'
 MILVUS_PORT = 19530
 nlp = spacy.load("en_core_web_lg")
 connections.connect(host="standalone", port="19530")
-collection_test = Collection(name="test_name")
 client = MilvusClient(uri=f"http://{MILVUS_HOST}:{MILVUS_PORT}", token="root:Milvus")
 
 
@@ -35,14 +34,7 @@ index_params = {
 #  index_name="busroutes_index"
 #)
 
-#collection_test.create_index(
-#  field_name="vector_field",
-#  index_params=index_params,
-#  index_name="busroutes_index"
-#)
-
 #collection_bus_routes.load()
-collection_test.load()
 
 @app.get("/collections/getvector1/{vector_id}")
 async def get_vector(
@@ -65,11 +57,9 @@ async def get_vector(
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-
 @app.get("/")
 async def func():
     try:
-        # List all collections
         collections = utility.list_collections()
         print(f"List all collections:\n", collections)
     except MilvusException as e:
@@ -81,52 +71,58 @@ async def func():
         response = await client.get("http://host.docker.internal:9099/hi")
         return response.json()
 
-
 @app.get("/get-entity-count/")
 async def get_entity_count():
     try:
-        count = collection.num_entities
-        return {"message": "Count of entities in collection", "count": count}
+       # count = collection.num_entities
+        return {"message": "Count of entities in collection", "count": 123123}
     except Exception as e:
         return {"message": "Error occurred while getting entity count:", "error": str(e)}
-
 
 @app.get("/test-milvus-connection/")
 async def test_milvus_connection():
     try:
-        # Check if connected to Milvus
+
         client = MilvusClient(uri=f"http://{MILVUS_HOST}:{MILVUS_PORT}", token="root:Milvus")
         status = client.get_collection_stats(collection_name="test_name")
         return {"message": "Connected to Milvus", "status": status}
     except Exception as e:
         return {"message": "Error occurred during Milvus connection:", "error": str(e)}
 
-class Item(BaseModel):
+class Insert(BaseModel):
+    name :List[str]
+
+class Search(BaseModel):
     name: List[str]
 
+class Update(BaseModel):
+    identifikator : List[int]
+    name : List[str]
+
+class Delete(BaseModel):
+    identifikator : List[int]
+
 @app.post("/insertText")
-async def insertText(text : Item):
+async def insertText(body : Insert):
     try:
         test_collection = "text_collection"
         collection = Collection(name=test_collection)
-        embedding = [nlp(name).vector for name in text.name]
-        name = text.name
+        embedding = [nlp(name).vector for name in body.name]
+        name = body.name
+       #ids = text.ids
+        #data = [ids,embedding,name]
         data = [embedding,name]
         mr = collection.insert(data)
         print(f"Insert result: {mr}")
-        
-        # Step 6: Flush to persist data
         collection.flush()
     except Exception as e:
         return {"message": "Error occurred during Milvus connection:", "error": str(e)}
     
 @app.post("/searchText")
-async def search_text(text: Item):
+async def searchText(body: Search):
     try:
-        # Generate vectors for each name in the input text
-        vectors = [nlp(name).vector for name in text.name]
+        vectors = [nlp(name).vector for name in body.name]
 
-        # Perform search on the collection
         res = client.search(
             collection_name="text_collection",
             data=vectors,
@@ -134,13 +130,11 @@ async def search_text(text: Item):
             search_params={"metric_type": "L2", "params": {}}
         )
 
-        # Extract search result IDs
         search_result_ids = [j["id"] for i in res for j in i]
         
         if not search_result_ids:
             raise HTTPException(status_code=404, detail="No search results found")
 
-        # Retrieve entities based on the search result IDs
         entities = client.get(
             collection_name="text_collection",
             ids=search_result_ids
@@ -153,6 +147,53 @@ async def search_text(text: Item):
         return returnValues
     except Exception as e:
         return {"message": "Error occurred during Milvus connection:", "error": str(e)}
+    
+@app.post("/upsertText")
+async def upsertText(body : Update):
+    try:
+
+        headers = {
+        "Content-Type": "application/json",
+        }
+        payload = {
+        "text": body.name[0],
+        "vectordatabaseid": body.identifikator
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.post("http://host.docker.internal:8081/Text/insert", headers=headers, json=payload)
+        if response:        
+            try:
+                collection_name="text_collection"
+                collection = Collection(collection_name)
+                collection.delete(f"ID in {body.identifikator}")
+                embedding = [nlp(name).vector for name in body.name]
+                name = body.name
+                data = [embedding,name]
+                mr = collection.insert(data)
+
+                print(f"Insert result: {mr}")
+                collection.flush()
+                return {"message":"Update completed."}
+            except Exception as e:
+                await client.post("http://host.docker.internal:8081/Text/delete", headers=headers, json=payload)
+                return {"message": "Transaction cancelled: \n Error occurred during Milvus connection:", "error": str(e)}
+        else: 
+            return {"message" : "Transaction failed"}
+    except Exception as e:
+        return {"message": "Error occurred during Milvus connection:", "error": str(e)}
+    
+@app.post("/deleteText")
+async def deleteText(body : Delete):
+    try:
+        collection_name="text_collection"
+        collection = Collection(collection_name)
+        collection.delete(f"ID in {body.identifikator}")
+        return {"message" : "Deleted entities."}
+    
+    except Exception as e:
+        return {"message": "Error occurred during Milvus connection:", "error": str(e)}
+        
+      
 
 if __name__ == "__main__":
     import uvicorn
